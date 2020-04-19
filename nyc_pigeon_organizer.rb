088a940b075661bbeb9b398e88e12d3554b84984
Lili Rodriguez <lilimrod@gmail.com>
2020-04-19 04:35:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,8 @@
 def nyc_pigeon_organizer(data)
  new_list = {}
  data.each do |keys, values|
-   values.each do |new_keys, new_values|
-     
+   values.each do |new_values, names|
+     names.each do |name|
+       if 
     
 end
