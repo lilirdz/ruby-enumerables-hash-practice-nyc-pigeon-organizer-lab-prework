e2f80cdb15e602b0641925a6d44fb111d41ce9e6
Lili Rodriguez <lilimrod@gmail.com>
2020-04-19 04:40:21 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,6 @@
 def nyc_pigeon_organizer(data)
  new_list = {}
- data.each do |keys, values|
+ data.each do |key, values|
    values.each do |new_values, names|
      names.each do |name|
        if new_list[name] == nil
@@ -9,7 +9,7 @@ def nyc_pigeon_organizer(data)
       if new_list[name][key] == nil
         new_list[name][key] = []
       end
-      new_list[name][key] >> key.to_s
+      new_list[name][key] >> new_values.to_s
     end
   end
  
